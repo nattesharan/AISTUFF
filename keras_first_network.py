0b06b8b74b09b8a1ff39d7d0ea4f9da3d169c743
Natte Sharan <nattesharan@gmail.com>
2017-09-09 23:38:44 +0530

    Added first NN

---
+++ keras_first_network.py
@@ -0,0 +1,29 @@
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense,Activation
+# fix random seed for reproducibility
+numpy.random.seed(7)
+'''
+Data set attibutes
+ 1. Number of times pregnant
+   2. Plasma glucose concentration a 2 hours in an oral glucose tolerance test
+   3. Diastolic blood pressure (mm Hg)
+   4. Triceps skin fold thickness (mm)
+   5. 2-Hour serum insulin (mu U/ml)
+   6. Body mass index (weight in kg/(height in m)^2)
+   7. Diabetes pedigree function
+   8. Age (years)
+   9. Class variable (0 or 1)
+'''
+dataset = numpy.loadtxt('pima-indians-diabetes.data.csv',delimiter=',')
+X = dataset[:,0:8]
+Y = dataset[:,8]
+model = Sequential()
+model.add(Dense(12,input_dim=8,activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+#compile the model
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+# fit the model
+model.fit(X,Y,epochs=150,batch_size=10)
+
